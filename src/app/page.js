883f8c3044ee6e4bad687f6e3b8a6f56a28c0bf1
Rifannvl <riffanafendi@gmail.com>
2024-10-19 11:40:30 +0700

    import component and add in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,48 @@
 import GridPattern from "@/components/ui/grid-pattern";
+import Link from "next/link";
+import Image from "next/image";
+import avatar from "@/assets/avatardemo.png";
+import GradualSpacing from "@/components/ui/gradual-spacing";
+import BlurFade from "@/components/ui/blur-fade";
+import Meteors from "@/components/ui/meteors";
 import { VelocityScroll } from "@/components/ui/scroll-based-velocity";
 import TextRevealByWord from "@/components/ui/text-reveal";
+import DockMenus from "@/components/ui/DockMenus";
 import { cn } from "@/lib/utils";
+import { CardHover } from "@/components/ui/CardHover";
+import { ThreeDCardDemo } from "@/components/ui/TriDCard";
 
 export default function Home() {
   return (
-    <>
-      <VelocityScroll
-        text={"web developer"}
-        default_velocity={5}
-        className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem] "
-      />
-
-      <div className="z-10 flex min-h-64 items-center justify-center rounded-lg border bg-white dark:bg-black">
+    <div>
+      <HeroSection />
+      <BlurFade>
+        <VelocityScroll
+          text={"web developer "}
+          default_velocity={3}
+          className="font-display text-center text-2xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-5xl md:leading-[4rem] "
+        />
+      </BlurFade>
+      <div className="z-10 flex  items-center justify-center bg-white dark:bg-black">
         <TextRevealByWord
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-"
+          text="Hi! I’m a frontend developer who loves building clean,
+                responsive, and user-friendly web interfaces. I enjoy working
+                with technologies like JavaScript, React, and CSS to create
+                seamless experiences that make users happy.
+                 While my main focus is on frontend, I also have experience in
+                backend development, which helps me understand the full picture
+                of a web project. I’m always eager to learn new things, explore
+                the latest trends in design, and work on projects that challenge
+                me to grow.
+                 Let’s connect and chat about web development, design, or
+                potential collaboration!
+                "
         />
       </div>
+
+      <DockMenus className="fixed bottom-12 left-1/2 -translate-x-1/2 z-50" />
+
+      <ThreeDCardDemo />
 
       <GridPattern
         width={20}
@@ -29,6 +53,53 @@ export default function Home() {
           "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
         )}
       />
-    </>
+    </div>
   );
+
+  function HeroSection() {
+    return (
+      <section className="container max-w-screen pt-10 grid grid-rows-2 items-center justify-center text-center ">
+        <div className="flex justify-center">
+          <Link href="/" className="hover:[&_img]:scale-90">
+            <Image
+              src={avatar}
+              alt="hero image"
+              width={200}
+              height={200}
+              className="rounded-full border p-1 transition-all"
+            />
+          </Link>
+        </div>
+
+        <div className="flex md:gap-4 md:items-center md:flex-row flex-col mb-6">
+          <div>
+            <GradualSpacing
+              className="font-display mb-2 lg:mb-4 text-xl sm:text-3xl font-bold -tracking-widest text-center text-black dark:text-white md:text-3xl lg:text-5xl"
+              text="Rifan Afendi"
+              duration={0.75}
+            />
+            <ul className="list-disc pl-2 flex flex-wrap">
+              <BlurFade>
+                <li className="mr-8 mb-1 text-sm md:text-base">
+                  Web Developer
+                </li>
+              </BlurFade>
+
+              <BlurFade delay={0.25}>
+                <li className="mr-8 mb-1 text-sm md:text-base">
+                  Remote worker
+                </li>
+              </BlurFade>
+
+              <BlurFade delay={0.5}>
+                <li className="mr-8 mb-1 text-sm md:text-base">
+                  <div className="flex gap-2">Based in Yogyakarta </div>
+                </li>
+              </BlurFade>
+            </ul>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
