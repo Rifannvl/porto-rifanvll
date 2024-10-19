6f5d72956f18d450e4892bcce7bb1faf021cf4a2
Rifannvl <riffanafendi@gmail.com>
2024-10-19 11:56:42 +0700

    add new prject

--- src/components/ui/TriDCard.jsx
+++ src/components/ui/TriDCard.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Avatar from "@/assets/ecmrs.png";
 import Travel from "@/assets/travel.png";
 import Smk from "@/assets/smk.png";
+import Yws from "@/assets/yws.png";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "./3d-card";
 import GradualSpacing from "./gradual-spacing";
@@ -153,6 +154,54 @@ export function ThreeDCardDemo() {
                   translateZ={20}
                   as={Link}
                   href="https://smkn1-simpangraya.vercel.app"
+                  target="__blank"
+                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                >
+                  next
+                </CardItem>
+              </div>
+            </CardBody>
+          </CardContainer>
+        </div>
+        <div>
+          <CardContainer className="inter-var">
+            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+              <CardItem
+                translateZ="50"
+                className="text-xl font-bold text-neutral-600 dark:text-white"
+              >
+                Yowis Coffe website (project pribadi)
+              </CardItem>
+              <CardItem
+                as="p"
+                translateZ="60"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              >
+                01-08-2024
+              </CardItem>
+              <CardItem translateZ="100" className="w-full mt-4">
+                <Image
+                  src={Yws}
+                  height="800"
+                  width="800"
+                  className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  alt="thumbnail"
+                />
+              </CardItem>
+              <div className="flex justify-between items-center mt-20">
+                <CardItem
+                  translateZ={20}
+                  as={Link}
+                  href="https://yowis-coffe.vercel.app"
+                  target="__blank"
+                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                >
+                  Try now →
+                </CardItem>
+                <CardItem
+                  translateZ={20}
+                  as={Link}
+                  href="https://yowis-coffe.vercel.app"
                   target="__blank"
                   className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                 >
