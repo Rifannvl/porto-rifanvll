afd109252894e748446adc839582d388f718ec93
Rifannvl <riffanafendi@gmail.com>
2024-10-20 00:30:25 +0700

    menambahkan button untuk download resume

--- src/components/ui/text-reveal.jsx
+++ src/components/ui/text-reveal.jsx
@@ -5,28 +5,19 @@ import { cn } from "@/lib/utils";
 
 export const TextRevealByWord = ({ text, className }) => {
   const targetRef = useRef(null);
-
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-  });
+  const { scrollYProgress } = useScroll({ target: targetRef });
   const words = text.split(" ");
 
   return (
-    <div className="w-full  ">
+    <div className="w-full container">
       <div ref={targetRef} className={cn("relative z-0 h-[115vh]", className)}>
-        <div
-          className={
-            "sticky top-0  grid grid-flow-row h-[50%] max-w-screen items-start bg-transparent px-[1rem] py-[5rem]"
-          }
-        >
+        <div className="sticky top-0 grid grid-flow-row h-[50%] max-w-screen items-start bg-transparent px-[1rem] py-[5rem]">
           <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-start lg:text-5xl md:text-4xl text-2xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
             About me
           </span>
           <p
             ref={targetRef}
-            className={
-              "flex flex-wrap text-xs font-light text-black/20 dark:text-white/20  md:text-sm py-4  lg:text-lg xl:text-xl"
-            }
+            className="flex flex-wrap text-xs font-light text-black/20 dark:text-white/20 md:text-sm py-4 lg:text-lg xl:text-xl"
           >
             {words.map((word, i) => {
               const start = i / words.length;
@@ -38,6 +29,13 @@ export const TextRevealByWord = ({ text, className }) => {
               );
             })}
           </p>
+          <a
+            href="/path/to/your/resume.pdf" // Ganti dengan path ke file resume kamu
+            download
+            className="mt-6 inline-block  max-w-52 px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200"
+          >
+            Download Resume/CV
+          </a>
         </div>
       </div>
     </div>
