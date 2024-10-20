6f14122fe9938cc7207df6d764ef5392ce66852a
Rifannvl <riffanafendi@gmail.com>
2024-10-20 13:10:40 +0700

    memperbaiki ukuran responsive

--- src/components/ui/CardHover.jsx
+++ src/components/ui/CardHover.jsx
@@ -1,15 +1,11 @@
 import { HoverEffect } from "@/components/ui/card-hover-effect";
 import { Target } from "lucide-react";
 import GradualSpacing from "./gradual-spacing";
+import { cn } from "@/lib/utils";
 
 export function CardHover() {
   return (
-    <div className="max-w-6xl mx-auto px-8">
-      <GradualSpacing
-        className="font-display mb-2 mt-5 lg:mb-4 text-xl sm:text-3xl font-bold -tracking-widest text-center text-neutral-700 dark:text-white md:text-3xl lg:text-5xl"
-        text="Project Rifan"
-        duration={0.75}
-      />
+    <div className={cn("mx-auto")}>
       <HoverEffect items={projects} />
     </div>
   );
@@ -17,39 +13,51 @@ export function CardHover() {
 export const projects = [
   {
     title: "React Js",
-    image: "https://img.icons8.com/?size=200&id=123603&format=png&color=000000",
+    image: "https://img.icons8.com/?size=100&id=123603&format=png&color=000000",
     link: "https://react.dev",
   },
   {
     title: "Next Js",
     image:
-      "https://img.icons8.com/?size=200&id=MWiBjkuHeMVq&format=png&color=000000",
+      "https://img.icons8.com/?size=100&id=MWiBjkuHeMVq&format=png&color=000000",
     link: "https://nextjs.org",
   },
   {
-    title: "Tailwind",
+    title: " Javascript",
     image:
-      "https://img.icons8.com/?size=200&id=CIAZz2CYc6Kc&format=png&color=000000",
-    link: "https://tailwindcss.com",
-  },
-  {
-    title: "Bootstrap",
-    image:
-      "https://img.icons8.com/?size=200&id=EzPCiQUqWWEa&format=png&color=000000",
-
-    link: "https://getbootstrap.com",
-  },
-  {
-    title: "Javascript",
-    image:
-      "https://img.icons8.com/?size=200&id=PXTY4q2Sq2lG&format=png&color=000000",
-
+      "https://img.icons8.com/?size=100&id=PXTY4q2Sq2lG&format=png&color=000000",
     link: "https://www.w3schools.com/js/",
   },
   {
+    title: "Node Express",
+    image: "https://img.icons8.com/?size=100&id=54087&format=png&color=000000",
+
+    link: "https://expressjs.com",
+  },
+  {
+    title: " Tailwind",
+    image:
+      "https://img.icons8.com/?size=100&id=CIAZz2CYc6Kc&format=png&color=000000",
+
+    link: "https://tailwindcss.com ",
+  },
+  {
+    title: "Bootsrap",
+    image:
+      "https://img.icons8.com/?size=100&id=EzPCiQUqWWEa&format=png&color=000000",
+
+    link: "https://getbootstrap.com ",
+  },
+  {
     title: "Html",
-    image: "https://img.icons8.com/?size=200&id=20909&format=png&color=000000",
+    image: "https://img.icons8.com/?size=100&id=20909&format=png&color=000000",
 
     link: "https://www.w3schools.com/html/",
+  },
+  {
+    title: "Css",
+    image: "https://img.icons8.com/?size=100&id=21278&format=png&color=000000",
+
+    link: "https://www.w3schools.com/css/",
   },
 ];
