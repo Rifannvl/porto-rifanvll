746020fbeb8b714679f2c1f54c6d46780d1d834b
Rifannvl <riffanafendi@gmail.com>
2024-10-20 20:14:59 +0700

    merubah margin

--- src/components/ui/ExpanCard.jsx
+++ src/components/ui/ExpanCard.jsx
@@ -181,7 +181,7 @@ export function ExpandableCard() {
             </motion.div>
           ))}
       </ul>
-      <div className="flex justify-start mt-4">
+      <div className="flex justify-start mt-5">
         <button
           onClick={handleToggleShowMore}
           className="px-4 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200"
