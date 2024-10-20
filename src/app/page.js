a5c7c7617f567bcce434ccf31ad76ed4e289cfc5
Rifannvl <riffanafendi@gmail.com>
2024-10-20 10:20:09 +0700

    menambahkan page service, dan memperbarui hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,4 @@
 import GridPattern from "@/components/ui/grid-pattern";
-import Link from "next/link";
-import Image from "next/image";
-import avatar from "@/assets/avatardemo.png";
 import GradualSpacing from "@/components/ui/gradual-spacing";
 import BlurFade from "@/components/ui/blur-fade";
 import { VelocityScroll } from "@/components/ui/scroll-based-velocity";
@@ -9,8 +6,9 @@ import TextRevealByWord from "@/components/ui/text-reveal";
 import DockMenus from "@/components/ui/DockMenus";
 import { cn } from "@/lib/utils";
 import { ExpandableCard } from "@/components/ui/ExpanCard";
-
 import { CardHover } from "@/components/ui/CardHover";
+import { BlurInText } from "@/components/ui/BlurInText";
+import { MagicCardDemo } from "@/components/ui/MagicCards";
 
 export default function Home() {
   return (
@@ -24,29 +22,26 @@ export default function Home() {
           className="font-display- text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[4rem] "
         />
       </BlurFade>
-      <div className="z-10 flex container items-center justify-center bg-white dark:bg-black">
+      <div className="z-10 flex flex-col items-center justify-center bg-white dark:bg-black container p-6">
         <TextRevealByWord
           text="Hi! I’m a frontend developer who loves building clean,
-                responsive, and user-friendly web interfaces. I enjoy working
-                with technologies like JavaScript, React, and CSS to create
-                seamless experiences that make users happy.
-                 While my main focus is on frontend, I also have experience in
-                backend development, which helps me understand the full picture
-                of a web project. I’m always eager to learn new things, explore
-                the latest trends in design, and work on projects that challenge
-                me to grow.
-                 Let’s connect and chat about web development, design, or
-                potential collaboration!
-                "
+          responsive, and user-friendly web interfaces. I enjoy working
+          with technologies like JavaScript, React, and CSS to create
+          seamless experiences that make users happy.
+          While my main focus is on frontend, I also have experience in
+          backend development, which helps me understand the full picture
+          of a web project. I’m always eager to learn new things, explore
+          the latest trends in design, and work on projects that challenge
+          me to grow.
+          Let’s connect and chat about web development, design, or
+          potential collaboration!"
         />
       </div>
 
       <CardHover />
-
       <DockMenus className="fixed bottom-12 left-1/2 -translate-x-1/2 z-50" />
-
       <ExpandableCard />
-
+      <MagicCardDemo />
       <GridPattern
         width={20}
         height={20}
@@ -62,7 +57,7 @@ export default function Home() {
   function HeroSection() {
     return (
       <section className="container max-w-screen pt-10 grid grid-rows-2 items-center justify-center text-center ">
-        <div className="flex justify-center">
+        {/* <div className="flex justify-center">
           <Link href="/" className="hover:[&_img]:scale-90">
             <Image
               src={avatar}
@@ -72,13 +67,13 @@ export default function Home() {
               className="rounded-full border p-1 transition-all"
             />
           </Link>
-        </div>
-
+        </div> */}
+        <BlurInText />
         <div className="flex md:gap-4 md:items-center md:flex-row flex-col mb-6">
           <div>
             <GradualSpacing
-              className="font-display mb-2 lg:mb-4 text-xl sm:text-3xl font-bold -tracking-widest text-center text-black dark:text-white md:text-3xl lg:text-5xl"
-              text="Rifan Afendi"
+              className="font-display mb-2 lg:mb-4 text-xl sm:text-3xl font-bold -tracking-widest text-center text-neutral-700 dark:text-white md:text-3xl lg:text-5xl"
+              text="Frontend Developer"
               duration={0.75}
             />
             <ul className="list-disc pl-2 flex ms-7 flex-wrap">
