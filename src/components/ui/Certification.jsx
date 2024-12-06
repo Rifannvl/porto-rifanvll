63479e8199d1c1f8469e3a82895ca8ef419d3ae0
Rifannvl <riffanafendi@gmail.com>
2024-12-06 22:47:38 +0700

    mengganti deskripsi dan merapikannya

--- src/components/ui/Certification.jsx
+++ src/components/ui/Certification.jsx
@@ -18,17 +18,22 @@ export default function Certification() {
       </div>
       <div className="flex h-[500px] w-full flex-col gap-4 lg:h-[250px] lg:flex-row">
         <a href="https://credsverse.com/credentials/7ef5499b-5be2-4087-aa0e-c43f90fc4067?preview=1">
-          <Card className="flex-col items-center justify-center text-xl px-10 text-center gap-3">
+          <Card className="flex-col items-center justify-center text-xl px-10 pb-6 text-center gap-3">
             <img
               src="https://img.icons8.com/?size=100&id=103650&format=png&color=000000"
               alt="Website Development Icon"
               className="mx-auto w-20" // Sesuaikan ukuran sesuai kebutuhan
             />
-            <p className="text-2xl text-neutral-800">Website Development</p>
+            <p className="text-2xl text-neutral-800 mx-auto  text-start">
+              Frontend Development
+            </p>
             <p className="text-xs mx-auto max-w-80 text-start">
-              Dapatkan keunggulan kompetitif dengan aplikasi web yang dibangun
-              di atas teknologi terbaru. Kami menghadirkan performa tinggi,
-              keamanan, dan skalabilitas untuk kebutuhan bisnis Anda.{" "}
+              The Frontend Development program at Neuversity teaches students
+              how to build interactive and responsive web applications. You'll
+              learn key technologies like HTML, CSS, JavaScript, and modern
+              frameworks such as React and Svelte. The program includes
+              practical experience to help you design user-friendly interfaces
+              and create web applications that meet real-world needs.{" "}
             </p>
           </Card>
         </a>
