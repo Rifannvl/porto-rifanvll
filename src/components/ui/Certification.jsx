4826f74753a302aebfe9353ebd969c44d7f83ab5
Rifannvl <riffanafendi@gmail.com>
2024-12-06 22:44:42 +0700

    membuat design dari komponent certification

--- src/components/ui/Certification.jsx
+++ src/components/ui/Certification.jsx
@@ -1,30 +1,37 @@
 import React from "react";
-import MagicCard from "./MagicCard";
 
 export default function Certification() {
+  function Card({ children, className }) {
+    return (
+      <div
+        className={`flex flex-col  rounded-xl bg-neutral-100 border items-center justify-center text-xl px-10 text-center gap-3 ${className}`}
+      >
+        {children}
+      </div>
+    );
+  }
   return (
-    <div>
-      <div className="mb-6">
-        <h2 className="text-5xl font-bold text-neutral-700">Services</h2>
-        <p>These services are what I can provide</p>
+    <div className="mt-10 mb-52">
+      <div className="">
+        <h2 className="text-5xl font-bold text-neutral-700">Certification</h2>
+        <p>Certification I have obtained </p>
       </div>
       <div className="flex h-[500px] w-full flex-col gap-4 lg:h-[250px] lg:flex-row">
-        <MagicCard
-          className="flex-col items-center justify-center text-xl px-10 text-center gap-3"
-          gradientColor={"#7e95fc55"}
-        >
-          <img
-            src="https://img.icons8.com/?size=100&id=103650&format=png&color=000000"
-            alt="Website Development Icon"
-            className="mx-auto w-20" // Sesuaikan ukuran sesuai kebutuhan
-          />
-          <p className="text-2xl text-neutral-800">Website Development</p>
-          <p className="text-xs mx-auto max-w-80 text-start">
-            Dapatkan keunggulan kompetitif dengan aplikasi web yang dibangun di
-            atas teknologi terbaru. Kami menghadirkan performa tinggi, keamanan,
-            dan skalabilitas untuk kebutuhan bisnis Anda.{" "}
-          </p>
-        </MagicCard>
+        <a href="https://credsverse.com/credentials/7ef5499b-5be2-4087-aa0e-c43f90fc4067?preview=1">
+          <Card className="flex-col items-center justify-center text-xl px-10 text-center gap-3">
+            <img
+              src="https://img.icons8.com/?size=100&id=103650&format=png&color=000000"
+              alt="Website Development Icon"
+              className="mx-auto w-20" // Sesuaikan ukuran sesuai kebutuhan
+            />
+            <p className="text-2xl text-neutral-800">Website Development</p>
+            <p className="text-xs mx-auto max-w-80 text-start">
+              Dapatkan keunggulan kompetitif dengan aplikasi web yang dibangun
+              di atas teknologi terbaru. Kami menghadirkan performa tinggi,
+              keamanan, dan skalabilitas untuk kebutuhan bisnis Anda.{" "}
+            </p>
+          </Card>
+        </a>
       </div>
     </div>
   );
